09d0af6ae5c088e1fea4908de61fd2ded238bf55
gongzheng <2897803069@qq.com>
2020-05-17 14:20:14 +0800

    eject前的提交

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import qs from 'qs';
+import './static/less/index.less';
+console.log(qs.parse('name=gz&age=24&lx=stu'))
+
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
