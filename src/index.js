7406b3f69f3f771fa08b2c5cc5065b03ea953b48
gongzheng <2897803069@qq.com>
2020-05-19 22:38:37 +0800

    函数声明式组件练习

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dialog from './component/Dialog';
-let root = document.getElementById('root')
-ReactDOM.render(<div>
-    {/* 注释：jsx中调取组件，只需要把组件当做一个标签调取使用即可（单闭合和双闭合都可以） */}
-    <Dialog con="哈哈哈"/>
-    <Dialog con="嘿嘿嘿" lx={2}>
-        {/* 属性值不是字符串，我们需要使用大括号包起来 */}
+import React from 'react'
+import ReactDOM from 'react-dom'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+import Dialog from './component/Dialog2'
+
+ReactDOM.render(<main>
+    <Dialog content='gz长得帅'/>
+    <Dialog type={2} content='你写的代码又报错了' />
+    <Dialog type="请登录" content={
+        <div>
+            <input type="text" className="form-control" placeholder="请输入用户名" />
+            <input type="password" className="form-control" placeholder="请输入用户密码" />
+        </div>
+    }>
+        <button className="btn btn-success">登录</button>
+        <button className="btn btn-danger">取消</button>
     </Dialog>
-</div>,root)
+</main>,document.getElementById('root'))
